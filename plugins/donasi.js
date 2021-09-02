28a3eeb55b2b96601c7cead5dbd8538de71ba444
Tritoapriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-09-02 21:01:14 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
+├ https://saweria.co/Trito
+├〔 Donasi Pembuat Scnya Juga Ya 〕
 ├ https://trakteer.id/ariffb/tip
 └────
 `.trim(), '© stikerin', 'Donasi', '.donasi')
