0683dcb8d16db54a3834911d7971012bf2dbb25e
Tritoapriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-09-02 15:12:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6283128734012'] // Letakan nomor kamu disini
+global.owner = ['6285888258313'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -22,14 +22,14 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'af527965711d015d',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.xyz': 'tritoapriansyah',
 }
 
 // Sticker WM
 global.packname = '❤️'
-global.author = 'ariffb'
+global.author = 'Trito'
 
 global.wait = '_*tunggu..*_'
 global.eror = '_*Server Error*_'
