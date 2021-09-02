559b1ef5589ebf1d145ff98668b6af1cd27949a1
Tritoapriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-09-02 15:14:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, '6288709645535', this.getName('6288709645535@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '6285888258313', this.getName('6285888258313@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
